1d664f650de6ad8be03e853e0ea1a81e1705bd20
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-03-11 16:56:36 -0300

    feat: adjustmaint component logo

--- src/pages/components/template/MenuLateral.tsx
+++ src/pages/components/template/MenuLateral.tsx
@@ -1,10 +1,18 @@
 import { MenuItem } from "./MenuItem";
 import { IconAdjustments, IconHome, IconNotifications } from "../icon";
+import Logo from "./Logo";
 
 export function MenuLateral() {
     return (
         <div>
             <aside>
+                <div  className={`
+                    flex flex-col items-center justify-center
+                    bg-gradient-to-r from-indigo-500 to-purple-800
+                    h-20 w-20
+                `}>
+                    <Logo />
+                </div>
                 <ul>
                     <MenuItem title="Inicio" url="/" icon={IconHome}/>
                     <MenuItem title="Ajuste" url="/Adjustments" icon={IconAdjustments}/>
