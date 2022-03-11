e6abf7713680bca313f2c2d62e5750f9ac7d2f2e
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-03-11 11:50:20 -0300

    feat: create new project Admin and configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <h1 className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-green-500 to-blue-500
+    `}>
+      Hello world!
+    </h1>
+  )
+}
