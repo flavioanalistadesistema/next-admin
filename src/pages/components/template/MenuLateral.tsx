302f79a93dc757fbbfbdd63c3b74585700fe22c4
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-03-11 18:32:39 -0300

    feat: menu Logout

--- src/pages/components/template/MenuLateral.tsx
+++ src/pages/components/template/MenuLateral.tsx
@@ -1,11 +1,11 @@
 import { MenuItem } from "./MenuItem";
-import { IconAdjustments, IconHome, IconNotifications } from "../icon";
+import { IconAdjustments, IconHome, IconLogout, IconNotifications } from "../icon";
 import Logo from "./Logo";
 
 export function MenuLateral() {
     return (
         <div>
-            <aside>
+            <aside className={`flex flex-col`}>
                 <div  className={`
                     flex flex-col items-center justify-center
                     bg-gradient-to-r from-indigo-500 to-purple-800
@@ -13,10 +13,20 @@ export function MenuLateral() {
                 `}>
                     <Logo />
                 </div>
-                <ul>
+                <ul className={`mb-64`}>
                     <MenuItem title="Inicio" url="/" icon={IconHome}/>
                     <MenuItem title="Ajuste" url="/Adjustments" icon={IconAdjustments}/>
                     <MenuItem title="Notificações" url="/Notifications" icon={IconNotifications}/>
+                </ul>
+                <ul className={`mt-64`}>
+                    <MenuItem 
+                        title="Sair" 
+                        icon={IconLogout}
+                        onClick={() => console.log('logout')}
+                        className={`
+                            text-red-600
+                            hover:bg-red-400 hover:text-white
+                        `}/>
                 </ul>
             </aside>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      flexGrow: {
+        '2': 2
+      }
+    },
   },
   plugins: [],
 }
