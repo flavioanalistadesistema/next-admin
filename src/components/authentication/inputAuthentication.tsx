39efd656b3ce2dfc7053ace68452187159cdfc87
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-03-15 16:48:56 -0300

    feat: create page authentication

--- src/components/authentication/inputAuthentication.tsx
+++ src/components/authentication/inputAuthentication.tsx
@@ -11,13 +11,18 @@ interface InputAuthenticationProps {
 export default function InputAuthentication(props: InputAuthenticationProps) {
     
     return props.notRender ? null : (
-        <div className={`flex flex-col `}>
+        <div className={`flex flex-col mt-4`}>
             <label>{props.label}</label>
             <input
                 type={props.type ?? 'text'}
                 value={props.value}
                 onChange={e => props.valueUpdate?.(e.target.value)}
                 required={props.required}
+                className={`
+                    px-3 py-3 rounded-lg bg-gray-200 mt-2
+                    border focus:border-blue-500 focus:bg-white
+                    focus:outline-none
+                `}
             ></input>
         </div>
     )
--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,17 +1,30 @@
 import { useState } from "react"
 import InputAuthentication from "../components/authentication/inputAuthentication"
-import { Layout } from "../components/template/Layout"
+import { IconGoogle } from "../components/icon"
 
 export default function Authentication(props) {
-    type typeModel= 'login' | 'cadastro'
-    
+    type typeModel = 'login' | 'cadastro'
+
     const [model, setModel] = useState<typeModel>('login')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    function Submit() {
+        if (model === 'login') {
+            console.log('login');
+        } else {
+            console.log('cadastrar');
+
+        }
+    }
     return (
-        <div>
-            <Layout title="Authentication" subtext="Esta é a tela de authetication">
+        <div className={`flex flex-col h-screen justify-center items-center`}>
+            <div className="w-1/2">
+                <h1 className={`
+                    text-xl font-bold mb-4
+                    `}>
+                    {model === 'login' ? 'Entre com sua conta' : 'Cadastra-se na plataforma'}
+                </h1>
                 <InputAuthentication
                     type="email"
                     label="Email"
@@ -32,7 +45,23 @@ export default function Authentication(props) {
                     valueUpdate={setPassword}
                     notRender={true}
                 />
-            </Layout>
+                <button onClick={Submit} className={`
+                    w-full bg-indigo-500 hover:bg-indigo-400
+                    text-white rounded-lg px-4 py-3 mt-6
+                `}>
+                    {model === 'login' ? 'Entrar' : 'Cadastrar'}
+                </button>
+
+                <hr className={`my-6 border-gray-300 w-full`} />
+
+                <button onClick={Submit} className={`
+                    flex justify-center items-center
+                    w-full bg-red-600 border-2 hover:bg-red-400 
+                    text-white rounded-lg px-2 py-3
+                `}>
+                    {IconGoogle(18)} Entrar com Google
+                </button>
+            </div>
         </div>
     )
 }
