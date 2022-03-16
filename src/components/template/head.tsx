6aa4bce6a4ecd46332c086434963edb14a03fed8
flavioanalistadesistema <flavioanalistadesistema@gmail.com>
2022-03-15 21:44:02 -0300

    responsive page - Feature/component authentication (#9)
    * feat: using context API
    
    * refactor: refactor code
    
    * feat: using hook for alter thema dark
    
    * feat: button alterThema
    
    * feat: layout button alter thema
    
    * feat: create component authentication
    
    Definition input text for authetication usuary
    
    * feat: create page authentication
    
    * feat: ajustment responsive page and link for register

--- src/components/template/head.tsx
+++ src/components/template/head.tsx
@@ -1,13 +1,19 @@
+import useAppData from "../../data/hook/useAppData"
+import ButtonAlterThema from "./ButtonAlterThema"
 import { Title } from "./Title"
 
-interface HeadProps{
+interface HeadProps {
     title: string,
     subtext: string
 }
 export function Head(props: HeadProps) {
+    const { thema, alterThema } = useAppData()
     return (
-        <div>
-            <Title title={props.title} subtext={props.subtext}/>
+        <div className={`flex`}>
+            <Title title={props.title} subtext={props.subtext} />
+            <div className={`flex flex-grow justify-end`}>
+                <ButtonAlterThema thema={thema} alterThema={alterThema} />
+            </div>
         </div>
     )
 }
