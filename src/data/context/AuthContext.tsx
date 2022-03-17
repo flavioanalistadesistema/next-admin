5d37e7e987c43f90a7e9f968803e79c482044d2b
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-03-17 17:34:07 -0300

    feat: authentication with google

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -10,25 +10,32 @@ interface AuthContextInterface {
 
 const AuthContext = createContext<AuthContextInterface>({})
 
-// async function userNormalize(userFirebase: firebase.User): Promise<User> {
-//     const token = await userFirebase.getIdToken()
+async function userNormalize(userFirebase: firebase.User): Promise<User> {
+    const token = await userFirebase.getIdToken()
 
-//     return {
-//         uid: userFirebase.uid,
-//         name: userFirebase.displayName,
-//         email: userFirebase.email,
-//         token,
-//         provider: userFirebase.providerData[0].providerId,
-//         imageUrl: userFirebase.photoURL
-//     }
-// }
+    return {
+        uid: userFirebase.uid,
+        name: userFirebase.displayName,
+        email: userFirebase.email,
+        token,
+        provider: userFirebase.providerData[0].providerId,
+        imageUrl: userFirebase.photoURL
+    }
+}
 
 export function AuthProvider(props) {
     const [user, setUser] = useState<User>(null)
 
     async function loginGoogle(){
-        console.log('Login do google');
-        router.push('/')
+        const resp = await firebase.auth().signInWithPopup(
+            new firebase.auth.GoogleAuthProvider()
+        )
+        
+        if(resp.user?.email) {
+            const  user = await userNormalize(resp.user)            
+            setUser(user)
+            router.push('/')
+        }
         
     }
 
