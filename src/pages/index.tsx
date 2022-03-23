7dcfd2295114066a66852b4885c9c93f1e0f4b34
flavioanalistadesistema <flavioanalistadesistema@gmail.com>
2022-03-23 17:27:54 -0300

    include repository (#16)
    * feat: protectiong routes
    
    * feat: one more layer of security
    
    * feat: save thema localStage
    
    * feat: authentication with email and password
    
    * refactor: refactor mesage error
    
    * Include repository project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
 import { Layout } from "../components/template/Layout";
 
 
 export default function Home() {
   return (
-    <Layout title="Página inicial" subtext="Estamos construindo uma nova pagina Admin">
-      <h3>Conteudo!!!</h3>
+    <Layout title="Página inicial" subtext="Esta é a pagina HOME/Admin">
+      <div className={`
+        flex justify-center items-center 
+        mt-10 ml-10
+        text-6xl
+      `}>
+          <a href="https://github.com/flavioanalistadesistema/next-admin">Segue o link do <strong>REPOSITÓRIO</strong></a>
+      </div>
     </Layout>
   )
 }
